faabf56b72add245486d67bf27e3b4993fe226c0
Tama (Masanori Fujta) <fujita@nulab-inc.com>
2019-09-04 01:08:35 +0900

    i18n

--- 20190906/resources/views/auth/register.blade.php
+++ 20190906/resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
       <div class="col-md-8">
         <div class="card">
-          <div class="card-header">{{ __('Register') }}</div>
+          <div class="card-header">{{ __('Register new user') }}</div>
 
           <div class="card-body">
             <form method="POST" action="{{ route('register') }}">
@@ -17,7 +17,7 @@
                 <div class="col-md-6">
                   <input id="name" type="text"
                          class="form-control @error('name') is-invalid @enderror" name="name"
-                         value="{{ old('name') }}" required autocomplete="name" autofocus>
+                         value="{{ old('name') }}"  autocomplete="name" autofocus>
 
                   @error('name')
                   <span class="invalid-feedback" role="alert">
@@ -50,7 +50,7 @@
                 <div class="col-md-6">
                   <input id="email" type="email"
                          class="form-control @error('email') is-invalid @enderror" name="email"
-                         value="{{ old('email') }}" required autocomplete="email">
+                         value="{{ old('email') }}" autocomplete="email">
 
                   @error('email')
                   <span class="invalid-feedback" role="alert">
@@ -67,7 +67,7 @@
                 <div class="col-md-6">
                   <input id="password" type="password"
                          class="form-control @error('password') is-invalid @enderror" name="password"
-                         required autocomplete="new-password">
+                         value="{{ old('password') }}" autocomplete="new-password">
 
                   @error('password')
                   <span class="invalid-feedback" role="alert">
@@ -82,9 +82,8 @@
                        class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                 <div class="col-md-6">
-                  <input id="password-confirm" type="password" class="form-control"
-                         name="password_confirmation" required
-                         autocomplete="new-password">
+                  <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
+                         value="{{ old('password-confirm') }}" autocomplete="new-password">
                 </div>
               </div>
 
